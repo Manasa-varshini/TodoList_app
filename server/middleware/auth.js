fa2ee20f15dc894d25c76b653eacdb29f3c3a855
Manasa-varshini <144097966+Manasa-varshini@users.noreply.github.com>
2025-06-29 20:59:34 +0530

    auth.js

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,15 +1,15 @@
-// middleware/auth.js
+
 
 const jwt = require('jsonwebtoken');
 
 const auth = (req, res, next) => {
-  const token = req.header('Authorization')?.split(' ')[1]; // Format: "Bearer <token>"
+  const token = req.header('Authorization')?.split(' ')[1]; 
   if (!token) {
     return res.status(401).json({ message: 'Access denied. No token provided.' });
   }
 
   try {
-    const decoded = jwt.verify(token, 'secretkey'); // Ensure 'secretkey' matches the one used in login
+    const decoded = jwt.verify(token, 'secretkey');
     req.user = decoded;
     next();
   } catch (err) {
